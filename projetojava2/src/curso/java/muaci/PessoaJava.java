4aeb31b53a3234a9da6c5142c0b80571bc80cf86
muacijose <muaci@outlook.com>
2022-07-08 21:56:47 -0300

    adicionanado o campo de idade na classe pessoa

--- projetojava2/src/curso/java/muaci/PessoaJava.java
+++ projetojava2/src/curso/java/muaci/PessoaJava.java
@@ -4,8 +4,15 @@ public class PessoaJava {
 	
 	private String nome;
 	private String sobrenome;
+	private int idade;
 	
 	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 	public String getNome() {
 		return nome;
 	}
