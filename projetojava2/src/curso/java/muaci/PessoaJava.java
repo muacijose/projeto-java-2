f9e0dd25978a28e7e8de6b2082533014760a07d6
muacijose <muaci@outlook.com>
2022-07-08 21:50:52 -0300

    alterçao e criação da classe pessoa

---
+++ projetojava2/src/curso/java/muaci/PessoaJava.java
@@ -0,0 +1,24 @@
+package curso.java.muaci;
+
+public class PessoaJava {
+	
+	private String nome;
+	private String sobrenome;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
+
+}
